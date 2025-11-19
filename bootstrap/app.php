d8ce29b960dc1dd65c9282e32da61f6fd7863a83
hariharasuthan.hb <hariharasuthan.hb@gmail.com>
2025-11-19 20:23:31 +0530

    auto notificaton on dite plan in app

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Jobs\SendDailyDietPlanNotificationsJob;
+use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -18,6 +20,12 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
+    ->withSchedule(function (Schedule $schedule): void {
+        $schedule->job(new SendDailyDietPlanNotificationsJob(), 'notifications')
+            ->name('daily-diet-plan-notifications')
+            ->dailyAt('05:00')
+            ->withoutOverlapping();
+    })
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
